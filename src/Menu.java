456114df9c5c399696cd6b20dd7dc9ed24e52383
Kigaru <jakubster9@gmail.com>
2018-04-25 00:23:03 +0100

    Refactored code to make it much cleaner

--- src/Menu.java
+++ src/Menu.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Menu {//TODO 1)seed randomizer, 2) swap letter accordingly
-    private String gibberish = "lvxswdfguhjknbiopearycqztm";
+    private String encryptedAlphabet = "lvxswdfguhjknbiopearycqztm";
     private final String alphabet = "abcdefghijklmnopqrstuvwxyz";
     private Scanner scan;
 
@@ -35,22 +35,16 @@ public class Menu {//TODO 1)seed randomizer, 2) swap letter accordingly
         scan.nextLine();
         System.out.print("What message do you want to encode: ");
         String code = scan.nextLine();
-        System.out.println("The encoded message is: \n"+ a2g(code)+"\n");
-    }
-    private String a2g(String code) {
-        String output = "";
-        System.out.println("String length: " + code.length());
-        output = avoidDupes(code, output, alphabet, gibberish);
-        return output;
+        System.out.println("The encoded message is: \n"+ converter(code, alphabet, encryptedAlphabet)+"\n");
     }
 
-    private String avoidDupes(String code, String output, String alphabet, String gibberish) {
-        StringBuilder outputBuilder = new StringBuilder(output);
+    private String converter(String code, String input, String algorithm) { //TODO enigma doesn't behave that way, because encryptedAlphabet changes on each letter input
+        StringBuilder outputBuilder = new StringBuilder();
         for(int i = 0; i < code.length(); i++) {
             char chara = code.toLowerCase().charAt(i);
             if(Character.isLetter(chara)) {
-                int letterArray = alphabet.indexOf(chara);
-                char convertedChar = gibberish.charAt(letterArray);
+                int letterArray = input.indexOf(chara);
+                char convertedChar = algorithm.charAt(letterArray);
                 String convertedLetter = String.valueOf(convertedChar);
                 outputBuilder.append(convertedLetter);
             }
@@ -59,20 +53,13 @@ public class Menu {//TODO 1)seed randomizer, 2) swap letter accordingly
                 outputBuilder.append(convertedLetter);
             }
         }
-        output = outputBuilder.toString();
-        return output;
+        return outputBuilder.toString();
     }
 
     private void decoder() {
         scan.nextLine();
         System.out.print("What message do you want to decode: ");
         String code = scan.nextLine();
-        System.out.println("The decoded message is: \n"+ g2a(code)+"\n");
-    }
-    private String g2a(String code) {
-        String output = "";
-        System.out.println("String length: " + code.length());
-        output = avoidDupes(code, output, gibberish, alphabet);
-        return output;
+        System.out.println("The decoded message is: \n"+ converter(code, encryptedAlphabet, alphabet)+"\n");
     }
 }
